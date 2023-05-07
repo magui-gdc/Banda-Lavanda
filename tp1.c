ec19061181ab8ec86abcbc5079a87d7cdc748fa7
mcabrera22 <martincho.cabrera22@gmail.com>
2023-05-07 19:48:49 -0300

    Punto 1, funcion A y B

--- tp1.c
+++ tp1.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(){
-  
-return 0
+
+
+return 0;
+}
+
+int largo(char* cadena){
+    int i = 0;
+
+    while (cadena[i]=! "/0")
+    {
+        i++;
+    }
+    return i;
+}
+
+int stringAInt(char* cadena){
+    int equivalenteInt = 0;
+    int i=0;
+    int numero;
+    while (cadena[i]=! "/0")
+    {   
+        numero = charAInt(cadena[i]);
+        equivalenteInt += numero * pow(10, i);
+        i++;
+    }
+
+    return equivalenteInt;
+}
+
+int charAInt(char caracter){
+    return caracter - 48;
 }
