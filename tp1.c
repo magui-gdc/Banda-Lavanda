b1c843ab64fde599c28f8141261b327527a4ba4b
Fernagarcia <fernagarcia@frba.utn.edu.ar>
2023-05-11 16:25:07 -0300

    1d

--- tp1.c
+++ tp1.c
@@ -1,20 +1,42 @@
 #include <stdio.h>
-#include <ctype.h>
+
 
 /* Functions */
 void convertirCadenaMayusculas(char* cadena);
 int largo(char* cadena);
 int stringAInt(char* cadena);
 int charAInt(char caracter);
+void eliminarCaracter(char* cadena, char caracter);
 
+/*
 int main(){
-    /* Cadena de prueba para las funciones */
+    Cadena de prueba para las funciones 
     char* cadena = "computacion";
+    char caracter = "c";
 
-    convertirCadenaMayusculas(cadena);
+    eliminarCaracter(cadena, caracter);
 
     return 0;
 }
+*/
+
+int main() {
+    char cadena[100], caracter;
+
+    printf("Ingrese la cadena: ");
+    gets(cadena);
+
+    printf("Ingrese el caracter que desea eliminar: ");
+    scanf("%c", &caracter);
+
+    eliminarCaracter(cadena, caracter);
+
+    printf("La cadena resultante es: %s\n", cadena);
+
+    return 0;
+}
+
+
 
 
 int largo(char* cadena){
@@ -27,25 +49,6 @@ int largo(char* cadena){
     return i;
 }
 
-int stringAInt(char* cadena){
-    int equivalenteInt = 0;
-    int i=0;
-    int numero;
-    int decena = 0;
-    int largoCadena = largo(cadena);
-    for(i=largoCadena-1; i>0; i--){   
-        numero = charAInt(cadena[i]);
-        equivalenteInt += numero * pow(10, decena);
-        decena++;
-    }
-
-    return equivalenteInt;
-}
-
-int charAInt(char caracter){
-    return caracter - 48;
-}
-
 int potencia(int numero, int potencia){
     int resultado = 1;
     if (potencia != 0){
@@ -56,6 +59,26 @@ int potencia(int numero, int potencia){
     return resultado;
 }
 
+int stringAInt(char* cadena){
+    int equivalenteInt = 0;
+    int i=0;
+    int numero;
+    int decena = 0;
+    int largoCadena = largo(cadena);
+    for(i=largoCadena-1; i>0; i--){   
+        numero = charAInt(cadena[i]);
+        equivalenteInt += numero * potencia(10, decena);
+        decena++;
+    }
+
+    return equivalenteInt;
+}
+
+int charAInt(char caracter){
+    return caracter - 48;
+}
+
+
 void convertirCadenaMayusculas (char* cadena) {
     int largoCadena = largo(cadena);
     int i;
@@ -64,4 +87,16 @@ void convertirCadenaMayusculas (char* cadena) {
             cadena[i] = cadena[i] - 32;
         }
     }
+}
+
+void eliminarCaracter(char* cadena, char caracter) {
+    int i, j = 0;
+
+    for (i = 0; cadena[i] != '\0'; i++) {
+        if (cadena[i] != caracter) 
+        {
+            cadena[j++] = cadena[i];
+        }   
+    }
+    cadena[j] = '\0';
 }
