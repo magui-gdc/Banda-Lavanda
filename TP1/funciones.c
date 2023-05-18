d89c3c16f2015ee224c593109cd7227304497517
Magali Gonzalez Di Carlo <mgonzalezdicarlo@frba.utn.edu.ar>
2023-05-18 19:30:06 -0300

    Punto concatenar cadenas arreglado

--- TP1/funciones.c
+++ TP1/funciones.c
@@ -5,7 +5,7 @@ int stringAInt(char*);
 int charAInt(char);
 void eliminarCaracter(char*, char);
 void modificarCadena (char*, char, int);
-char* concatenarCadenas(char*, char*);
+void concatenarCadenas(char*, char*, char*);
 
 int largo(char* cadena){
     int i = 0;
@@ -68,46 +68,28 @@ void eliminarCaracter(char* cadena, char caracter) {
     }
     cadena[j] = '\0';
 }
-/*
-char* concatenarCadenas(char* cadena1, char* cadena2) {
-    int largoCadena1 = largo(cadena1);
-    int largoCadenaFinal = largoCadena1 + largo(cadena2);
-    char cadenaFinal[largoCadenaFinal];
-    int i = 0;
-    for (i; i <= largoCadenaFinal; i++){
-        if (i < largoCadena1){
-            cadenaFinal[i] = cadena1[i];
-        } else {
-            cadenaFinal[i] = cadena2[i];
-        }
-    }
-    return cadenaFinal;
-}
-*/
-/*          TAMPOCO FUNCIONA!!
-char* concatenarCadenas(char* cadena1, char* cadena2) {
-    int largoCadena1 = largo(cadena1);
-    int largoCadenaFinal = largoCadena1 + largo(cadena2);
-    int i = 0;
-    int j = 0;
 
-    char cadenaFinal[largoCadenaFinal];
-   
-   char* aux = &cadenaFinal
+void concatenarCadenas (char* cadena1, char* cadena2, char* cadenaFinal){
 
-    while (cadena1[i] != '\0') {
+    int i = 0, j = 0;
+
+    while (cadena1[i] != '\0')
+    {
         cadenaFinal[i] = cadena1[i];
         i++;
     }
-
-    while (cadena2[j] != '\0') {
+    
+    while (cadena2[j] != '\0')
+    {
         cadenaFinal[i] = cadena2[j];
         i++;
         j++;
     }
-    return aux;
+
+    cadenaFinal[i + j] = '\0';
+    
 }
-*/
+
 void modificarCadena (char* cadena, char caracter, int posicion) {
     int i = 0;
     int largoCadena = largo(cadena);
--- TP1/menu.c
+++ TP1/menu.c
@@ -7,7 +7,7 @@ int main() {
     int opcion;
     char cadena[100];
     char cadena2[100];
-    char cadena3[100];
+    char cadena3[200];
     char caracter;
     int posicion;
 
@@ -34,44 +34,45 @@ int main() {
     {
     case 1:
         printf("\nCALCULO DE LONGITUD\nIngrese cadena:\n");
-        scanf("%99s", cadena);
+        scanf("%s", cadena);
         printf("El largo es: \n");
         printf("%d", largo(cadena));
         goto menu;
     
     case 2:
         printf("\nCALCULO DE EQUIVALENTE NUMERICA\nIngrese cadena:");
-        scanf(" %99s", cadena);
+        scanf(" %s", cadena);
         printf("%d", stringAInt(cadena));
         goto menu;
 
     case 3:
         printf("\nCONVERTIR A MAYUSCULAS\nIngrese cadena:");
-        scanf(" %99s", cadena);
+        scanf(" %s", cadena);
         convertirCadenaMayusculas(cadena);
         printf("La cadena en mayusculas es: %s",cadena);
         goto menu;
 
     case 4:
         printf("\nELIMINAR EL CARACTER DADO\nIngrese cadena:");
-        scanf(" %99s", cadena);
+        scanf(" %s", cadena);
         printf("\nIngrese caracter a eliminar:");
         scanf(" %c", &caracter);
         eliminarCaracter(cadena, caracter);
         printf("La cadena con el caracter %c eliminado seria: %s",caracter,cadena);
         goto menu;
 
-    /*case 5:      // NO FUNCIONA!!!!!!!! 
+    case 5:      
         printf("\nCONCATENAR DOS CADENAS\nIngrese primera cadena:");
-        scanf(" %99s", cadena);
+        scanf(" %s", cadena);
         printf("\nIngrese segunda cadena:");
-        scanf(" %99s", cadena2);
-        printf("La cadena %s y %s concatenadas serian: %s",cadena,cadena2,concatenarCadenas(cadena,cadena2));
+        scanf(" %s", cadena2);
+        concatenarCadenas(cadena,cadena2,cadena3);
+        printf("La cadena %s y %s concatenadas serian: %s",cadena,cadena2,cadena3);
         goto menu;     
-*/
+
     case 6:
         printf("\nMODIFICAR CADENA\nIngrese cadena:");
-        scanf(" %99s", cadena);
+        scanf(" %s", cadena);
         printf("\nIngrese caracter a agregar:");
         scanf(" %c", &caracter);
         printf("\nIngrese posicion a agregar caracter:");
